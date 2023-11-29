221a52c768f682d143ba52213b1d29e527debde2
Gabriel Erick <gabrielmmve13@gmail.com>
2023-11-28 23:11:18 -0300

    alteração do carrosel e sobre nos

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true, // Ativar o autoplay
+    autoplayTimeout: 3000, // Definir o intervalo de tempo em milissegundos (por exemplo, 3000ms = 3 segundos)
     responsive:{
         0:{
-            items:1
+            items: 1
         },
     }
-})
+});
