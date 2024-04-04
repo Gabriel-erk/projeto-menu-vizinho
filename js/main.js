8b934cb0c13cf270af1021594a5977d5142ac00e
Gabriel Erick <gabrielmmve13@gmail.com>
2024-04-04 14:03:02 -0300

    formatção do cabecalho e topicos cardapio

--- js/main.js
+++ js/main.js
@@ -17,21 +17,14 @@ $('.owl-carousel').owlCarousel({
 
 let contadorCarrinho = document.getElementById('contador-carrinho');
 
-//Essa linha de código está obtendo o valor associado à chave 'contadorCarrinho' armazenado no localStorage do navegador e atribuindo-o à variável valorAtual - o metodo getItem, serve para localizar o valor no local storage do item na chave = (), se nao houver nada, será retornado null
 let valorAtual = localStorage.getItem('contadorCarrinho');
-
-// se houver um valor diferente de null, ou seja, se tiver algum valor no carrinho, atualize o contador com este valor armazenado 
 
 if (valorAtual !== null) {
     contadorCarrinho.textContent = valorAtual;
 }
 
-// Seleciona todos os botões de compra
 let botoesCompra = document.querySelectorAll('.texto-lanche a');
 
-// adicionando um ouvinte de evento do tipo clique a cada botão de compra e atualizando o valor do carrinho
-
-// utilizando forEach pois sao multiplos botoes de compra que quero adicionar esta função
 botoesCompra.forEach(function(botao) {
     botao.addEventListener('click', function() {
         let valorAtual = parseInt(contadorCarrinho.textContent);
@@ -42,8 +35,6 @@ botoesCompra.forEach(function(botao) {
         localStorage.setItem('contadorCarrinho', valorAtual);
     });
 });
-
-// verifique se há itens no carrinho quando a pagina for carregada - se houver o contador aparece
 
 window.addEventListener('load', function(){
     let valorAtual = parseInt(contadorCarrinho.textContent);
