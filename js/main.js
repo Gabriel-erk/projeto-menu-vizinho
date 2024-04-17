23cd5e96d6dff4135719f780453264f6285b07ed
Gabriel <gabrielmmve13@gmail.com>
2024-04-16 22:14:49 -0300

    concluindo barra lateral

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    dots: false,
-    autoplay: true, // Ativar o autoplay
-    autoplayTimeout: 3000, // Definir o intervalo de tempo em milissegundos (por exemplo, 3000ms = 3 segundos)
-    autoplaySpeed: 900,
-    responsive:{
-        0:{
-            items: 1
-        },
-    }
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: false,
+  autoplay: true, // Ativar o autoplay
+  autoplayTimeout: 3000, // Definir o intervalo de tempo em milissegundos (por exemplo, 3000ms = 3 segundos)
+  autoplaySpeed: 900,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const iconeMenu = document.getElementById("iconeMenu");
+  const fecharMenu = document.getElementById("fechar-menu");
+  const menuLateral = document.getElementById("menuLateral");
+
+  iconeMenu.addEventListener('click', function() {
+    menuLateral.style.left = '0px';
+  })
+
+  fecharMenu.addEventListener('click', function() {
+    menuLateral.style.left = '-500px';
+  })
+
+  
+
 });
